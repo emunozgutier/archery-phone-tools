00750cb021d2690cd4655a8ed86c52f1c1e08487
Eduardo Munoz-Gutierrez <emunozgutier@gmail.com>
2026-05-26 21:18:32 -0700

    chore: release v2026.22.22

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => {
@@ -11,7 +12,8 @@ export default defineConfig(({ command }) => {
     },
     plugins: [
       react(),
-      babel({ presets: [reactCompilerPreset()] })
+      babel({ presets: [reactCompilerPreset()] }),
+      ...(command === 'serve' ? [basicSsl()] : [])
     ],
   }
 })
